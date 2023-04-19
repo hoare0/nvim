b001db73b5244a9ce2456aebbc8001fc6c995936
semtor <semtorcc@gmail.com>
2023-04-19 22:25:59 +0800

    修改部分按键，取消保存时自动格式化

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "<c-p>", "<cmd>BufferLineCyclePrev<cr>")
+keymap.set("n", "<c-n>", "<cmd>BufferLineCycleNext<cr>")
+keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
