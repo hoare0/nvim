d4eeb8f6f9bccac91135093cadcf39dd0db8307e
TheFerry <1771391789@qq.com>
2024-08-14 22:55:44 +0800

    feat: ✨ 禁用clangd对proto文件的服务

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,21 @@
 return {
   { "tikhomirov/vim-glsl" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      server = {
+        clangd = {},
+      },
+      setup = {
+        clangd = function()
+          require("lspconfig").clangd.setup({
+            filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+          })
+          return true
+        end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+      },
+    },
+  },
 }
